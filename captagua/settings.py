f931ccfd05f36c50e4c2af49931b800a03648ad9
enzoalencar <enzo14alencar@gmail.com>
2022-11-20 09:54:49 -0300

    responsive update

--- captagua/settings.py
+++ captagua/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q)%+xw3v54ks6muc@-2#3+qy+c+wv%0lmx#s2+-h*b1du+zfrb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io']
 
 
 # Application definition
